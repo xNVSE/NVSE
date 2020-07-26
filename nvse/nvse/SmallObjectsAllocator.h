19c86839405c2932cf825f2c31e368ad23eca361
korri123 <korri123@gmail.com>
2020-07-26 22:36:36 +0000

    Replaced SmallObjectsAllocator with faster impl

--- nvse/nvse/SmallObjectsAllocator.h
+++ nvse/nvse/SmallObjectsAllocator.h
@@ -1,51 +1,21 @@
 #pragma once
-#include <algorithm>
-#include <typeinfo>
+#include "MemoryPool.h"
 
 namespace SmallObjectsAllocator
 {
-	template <class T, std::size_t Count>
+	template <class T, std::size_t C>
 	class Allocator
 	{
-		T* memBlock_ = static_cast<T*>(::operator new (sizeof(T) * Count));
-		bool allocated_[Count] = { false };
-		std::size_t nextPos_ = 0;
+		MemoryPool<T, sizeof(T)* C> pool_;
 	public:
-		Allocator() = default;
-
-		~Allocator()
-		{
-			::operator delete(memBlock_);
-		}
-
 		T* Allocate()
 		{
-			for (auto i = nextPos_; i < Count; i++)
-			{
-				if (!allocated_[i])
-				{
-					nextPos_ = i + 1;
-					allocated_[i] = true;
-					return &memBlock_[i];
-				}
-			}
-			nextPos_ = -1;
-			_MESSAGE("%s has ran out of small objects", typeid(T).name());
-			ShowErrorMessageBox("SmallObjectsAllocator Warning");
-			return static_cast<T*>(::operator new(sizeof(T)));
+			return pool_.allocate();
 		}
 
 		void Free(T* ptr)
 		{
-			ptrdiff_t pos = ptr - memBlock_;
-
-			if (pos >= 0 && pos < Count)
-			{
-				nextPos_ = std::min<unsigned int>(static_cast<unsigned int>(pos), nextPos_);
-				allocated_[pos] = false;
-				return;
-			}
-			::operator delete(ptr);
+			pool_.deallocate(ptr);
 		}
 
 		void Free(void* ptr)
