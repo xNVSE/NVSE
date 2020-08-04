f472c7da0ce65b42d65f5bd54f4b22b80ff0522c
korri123 <korri123@gmail.com>
2020-08-04 00:16:40 +0000

    Added replacement of sprintf_s
    Should speed up Just Visual Objectives

--- nvse/nvse/GameAPI.cpp
+++ nvse/nvse/GameAPI.cpp
@@ -7,6 +7,7 @@
 #include "GameScript.h"
 #include "Hooks_Other.h"
 #include "StringVar.h"
+#include "printf.h"
 
 #if NVSE_CORE
 #include "Hooks_Script.h"
@@ -1052,7 +1053,7 @@ bool ExtractFormattedString(FormatStringArgs& args, char* buffer)
 				else
 				{			
 					char formID[9];
-					sprintf_s(formID, 9, "%08X", form->refID);
+					snprintf(formID, 9, "%08X", form->refID);
 					fmtString.insert(strIdx, formID);
 				}
 			}
@@ -1317,7 +1318,7 @@ bool ExtractFormattedString(FormatStringArgs& args, char* buffer)
 		}
 	}
 
-	if (sprintf_s(buffer, kMaxMessageLength - 2, fmtString.c_str(), f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10], f[11], f[12], f[13], f[14], f[15], f[16], f[17], f[18], f[19]) > 0)
+	if (snprintf(buffer, kMaxMessageLength - 2, fmtString.c_str(), f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7], f[8], f[9], f[10], f[11], f[12], f[13], f[14], f[15], f[16], f[17], f[18], f[19]) > 0)
 	{
 		buffer[kMaxMessageLength-1] = '\0';
 		return true;
