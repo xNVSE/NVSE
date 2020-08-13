ffccf2c815903ebcb90d3fd02c5ef0b2e6c9ae5f
Jef <12400205+jazzisparis@users.noreply.github.com>
2020-08-13 22:11:55 +0000

    Refactoring Tile:: functions.

--- nvse/nvse/GameUI.cpp
+++ nvse/nvse/GameUI.cpp
@@ -52,57 +52,67 @@ Menu * InterfaceManager::TempMenuByType(UInt32 menuType)
 	return NULL;
 }
 
+const char kMenuNames[] =
+"MessageMenu\0InventoryMenu\0StatsMenu\0HUDMainMenu\0LoadingMenu\0ContainerMenu\0DialogMenu\0SleepWaitMenu\0StartMenu\0\
+LockpickMenu\0QuantityMenu\0MapMenu\0BookMenu\0LevelUpMenu\0RepairMenu\0RaceSexMenu\0CharGenMenu\0TextEditMenu\0BarterMenu\0\
+SurgeryMenu\0HackingMenu\0VATSMenu\0ComputersMenu\0RepairServicesMenu\0TutorialMenu\0SpecialBookMenu\0ItemModMenu\0LoveTesterMenu\0\
+CompanionWheelMenu\0TraitSelectMenu\0RecipeMenu\0SlotMachineMenu\0BlackjackMenu\0RouletteMenu\0CaravanMenu\0TraitMenu";
+const UInt32 kMenuIDs[] =
+{
+	kMenuType_Message, kMenuType_Inventory, kMenuType_Stats, kMenuType_HUDMain, kMenuType_Loading, kMenuType_Container,
+	kMenuType_Dialog, kMenuType_SleepWait, kMenuType_Start, kMenuType_LockPick, kMenuType_Quantity, kMenuType_Map, kMenuType_Book,
+	kMenuType_LevelUp, kMenuType_Repair, kMenuType_RaceSex, kMenuType_CharGen, kMenuType_TextEdit, kMenuType_Barter, kMenuType_Surgery,
+	kMenuType_Hacking, kMenuType_VATS, kMenuType_Computers, kMenuType_RepairServices, kMenuType_Tutorial, kMenuType_SpecialBook,
+	kMenuType_ItemMod, kMenuType_LoveTester, kMenuType_CompanionWheel, kMenuType_TraitSelect, kMenuType_Recipe, kMenuType_SlotMachine,
+	kMenuType_Blackjack, kMenuType_Roulette, kMenuType_Caravan, kMenuType_Trait
+};
+TileMenu **g_tileMenuArray = NULL;
+UnorderedMap<const char*, UInt32> s_menuNameToID(0x40);
+
 // Split component path into "top-level menu name" and "everything else".
 // Path is of format "MenuType/tile/tile/..." following hierarchy defined in menu's xml.
 // Returns pointer to top-level menu or NULL.
 // pSlashPos is set to the slash character after the top-level menu name.
 TileMenu* InterfaceManager::GetMenuByPath(const char * componentPath, const char ** pSlashPos)
 {
-	// get menu name - stored by game as "&MENUNAME;" so need to fix it up
-	const char* slashPos = strpbrk(componentPath, "\\/");
-	if(slashPos)
+	if (s_menuNameToID.Empty())
 	{
-		std::string menuName("&");
-		menuName.append(componentPath, (slashPos - componentPath));
-		menuName.append(";");
-
-		UInt32 menuType = Tile::TraitNameToID(menuName.c_str());
-		if((menuType >= kMenuType_Min) && (menuType <= kMenuType_Max))
+		g_tileMenuArray = *(TileMenu***)0x11F350C;
+		const char *strPos = kMenuNames;
+		UInt32 count = 0;
+		do
 		{
-			TileMenu * tileMenu = g_TileMenuArray->Get(menuType - kMenuType_Min);
-			if(tileMenu)
-			{
-				*pSlashPos = slashPos;
-				return tileMenu;
-			}
+			s_menuNameToID[strPos] = kMenuIDs[count];
+			strPos = StrEnd(strPos) + 1;
 		}
+		while (++count < 36);
 	}
 
-	return NULL;
+	char *slashPos = SlashPos(componentPath);
+	if (slashPos) *slashPos = 0;
+	*pSlashPos = slashPos;
+	UInt32 menuID = s_menuNameToID.Get(componentPath);
+	return menuID ? g_tileMenuArray[menuID - kMenuType_Min] : NULL;
 }
 
 Tile::Value* InterfaceManager::GetMenuComponentValue(const char * componentPath)
 {
 	// path is of format "MenuType/tile/tile/.../traitName" following hierarchy defined in menu's xml
-	const char* slashPos = NULL;
-	TileMenu * tileMenu = GetMenuByPath(componentPath, &slashPos);
-	if(tileMenu && slashPos)
-	{
+	const char *slashPos;
+	TileMenu *tileMenu = GetMenuByPath(componentPath, &slashPos);
+	if (tileMenu && slashPos)
 		return tileMenu->GetComponentValue(slashPos + 1);
-	}
 	return NULL;
 }
 
 Tile* InterfaceManager::GetMenuComponentTile(const char * componentPath)
 {
 	// path is of format "MenuType/tile/tile/.../tile" following hierarchy defined in menu's xml
-	const char* slashPos = NULL;
-	TileMenu * tileMenu = GetMenuByPath(componentPath, &slashPos);
-	if(tileMenu && slashPos)
-	{
+	const char *slashPos;
+	TileMenu *tileMenu = GetMenuByPath(componentPath, &slashPos);
+	if (tileMenu && slashPos)
 		return tileMenu->GetComponentTile(slashPos + 1);
-	}
-	return NULL;
+	return tileMenu;
 }
 
 void Debug_DumpMenus(void)
