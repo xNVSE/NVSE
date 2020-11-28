d790ddeeecb2e6f0480a99eeedc478f46a6e802c
korri123 <korri123@gmail.com>
2020-11-28 22:18:41 +0000

    Update nvse_version.h

--- nvse/nvse/nvse_version.h
+++ nvse/nvse/nvse_version.h
@@ -3,9 +3,9 @@
 
 // these have to be macros so they can be used in the .rc
 #define NVSE_VERSION_INTEGER		5
-#define NVSE_VERSION_INTEGER_MINOR	2
-#define NVSE_VERSION_INTEGER_BETA	8
-#define NVSE_VERSION_VERSTRING		"0, 5, 2, 8"
+#define NVSE_VERSION_INTEGER_MINOR	3
+#define NVSE_VERSION_INTEGER_BETA	0
+#define NVSE_VERSION_VERSTRING		"0, 5, 3, 0"
 #define NVSE_VERSION_PADDEDSTRING	"0005"
 
 // build numbers do not appear to follow the same format as with oblivion
