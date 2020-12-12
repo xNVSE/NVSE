facf94c8db4aa6602dcb53cdc1d915701fdb410e
korri123 <korri123@gmail.com>
2020-12-12 02:43:09 +0000

    Log reason why plugin doesn't load

--- nvse/nvse/PluginManager.cpp
+++ nvse/nvse/PluginManager.cpp
@@ -416,6 +416,22 @@ bool PluginManager::FindPluginDirectory(void)
 	return result;
 }
 
+//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
+std::string GetLastErrorAsString()
+{
+	//Get the error message, if any.
+	DWORD errorMessageID = GetLastError();
+	if (errorMessageID == 0)
+		return std::string(); //No error message has been recorded
+	LPSTR messageBuffer = nullptr;
+	const size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+	  nullptr, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, nullptr);
+	std::string message(messageBuffer, size);
+	//Free the buffer.
+	LocalFree(messageBuffer);
+	return message;
+}
+
 bool PluginManager::InstallPlugin(std::string pluginPath)
 {
 	_MESSAGE("checking plugin %s", pluginPath.c_str());
@@ -489,7 +505,7 @@ bool PluginManager::InstallPlugin(std::string pluginPath)
 	}
 	else
 	{
-		_ERROR("couldn't load plugin %s", pluginPath.c_str());
+		_ERROR("couldn't load plugin %s (win32 error code: %d message: \"%s\")", pluginPath.c_str(), GetLastError(), GetLastErrorAsString().c_str());
 		return false;
 	}
 }
