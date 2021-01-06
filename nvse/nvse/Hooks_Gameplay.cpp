604d0c5cd8b793c0799fa0fd2034d70e0b2b466c
korri123 <korri123@gmail.com>
2021-01-06 13:31:28 +0000

    Add xNVSE version console print on game startup

--- nvse/nvse/Hooks_Gameplay.cpp
+++ nvse/nvse/Hooks_Gameplay.cpp
@@ -66,7 +66,7 @@ static void HandleMainLoopHook(void)
 		g_mainThreadID = GetCurrentThreadId();
 		
 		PluginManager::Dispatch_Message(0, NVSEMessagingInterface::kMessage_DeferredInit, NULL, 0, NULL);
-
+		Console_Print("xNVSE %d.%d.%d", NVSE_VERSION_INTEGER, NVSE_VERSION_INTEGER_MINOR, NVSE_VERSION_INTEGER_BETA);
 	}
 	PluginManager::Dispatch_Message(0, NVSEMessagingInterface::kMessage_MainGameLoop, nullptr, 0, nullptr);
 
