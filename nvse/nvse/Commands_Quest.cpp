40a171c12cd81515214369c923a034ab52dbe145
Anh <demonxranger8@gmail.com>
2021-12-15 15:29:40 +0700

    GetNthQuestObjective, GetQuestObjectiveCount, GetCurrentObjective
    Ported FOSE-only functions

---
+++ nvse/nvse/Commands_Quest.cpp
@@ -0,0 +1,53 @@
+#include "Commands_Quest.h"
+#include "GameForms.h"
+#include "GameObjects.h"
+#include "GameAPI.h"
+
+bool Cmd_GetQuestObjectiveCount_Execute(COMMAND_ARGS)
+{
+	*result = 0;
+
+	TESQuest* pQuest = NULL;
+	if (ExtractArgs(EXTRACT_ARGS, &pQuest)) {
+		tList<TESQuest::LocalVariableOrObjectivePtr> list = pQuest->lVarOrObjectives;
+		UInt32 count = list.Count();
+		*result = count;
+		if (IsConsoleMode())
+			Console_Print("%s has %d objectives", GetFullName(pQuest), count);
+	}
+
+	return true;
+}
+
+bool Cmd_GetNthQuestObjective_Execute(COMMAND_ARGS)
+{
+	*result = 0;
+
+	TESQuest* pQuest = NULL;
+	UInt32 nIndex = 0;
+	if (ExtractArgs(EXTRACT_ARGS, &pQuest, &nIndex)) {
+		if (!pQuest) return true;
+
+		tList<TESQuest::LocalVariableOrObjectivePtr> list = pQuest->lVarOrObjectives;
+		BGSQuestObjective* pObjective = (BGSQuestObjective*)list.GetNthItem(nIndex);
+		if (pObjective) {
+			*result = pObjective->objectiveId;
+		}
+	}
+	return true;
+}
+
+bool Cmd_GetCurrentObjective_Execute(COMMAND_ARGS)
+{
+	UInt32* refResult = (UInt32*)result;
+	*refResult = 0;
+
+	PlayerCharacter* pPC = PlayerCharacter::GetSingleton();
+	tList<BGSQuestObjective> list = pPC->questObjectiveList;
+	BGSQuestObjective* pCurObjective = (BGSQuestObjective*)list.GetFirstItem();
+	if (pCurObjective) {
+		*refResult = pCurObjective->objectiveId;
+	}
+
+	return true;
+}
---
+++ nvse/nvse/Commands_Quest.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CommandTable.h"
+#include "ParamInfos.h"
+
+static ParamInfo kParams_QuestInteger[2] =
+{
+	{	"quest", kParamType_Quest,		0	},
+	{	"index", kParamType_Integer,	0	}
+};
+
+
+DEFINE_CMD_ALT(GetQuestObjectiveCount, GetObjectiveCount, returns the number of objectives for the specified quest, 0, 1, kParams_OneQuest);
+DEFINE_CMD_ALT(GetNthQuestObjective, GetNthObjective, returns the objectiveID of the Nth objective of the quest, 0, 2, kParams_QuestInteger);
+DEFINE_CMD_ALT(GetCurrentObjective, GetCurObjID, returns the objectiveID of the current objective, 0, 0, NULL);
