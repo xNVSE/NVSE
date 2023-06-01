e4bfb7d02c7c527555004d37562f6714bfd438d8
Demorome <69116996+Demorome@users.noreply.github.com>
2023-06-01 19:21:36 -0300

    Revert "6.3.1"
    This reverts commit a53048e9d6b2346cb2cd41205b364b3901a80b2b.

--- nvse/nvse/nvse_version.h
+++ nvse/nvse/nvse_version.h
@@ -5,8 +5,8 @@
 #define ALPHA_MODE 0
 #define NVSE_VERSION_INTEGER		6
 #define NVSE_VERSION_INTEGER_MINOR	3
-#define NVSE_VERSION_INTEGER_BETA	1
-#define NVSE_VERSION_VERSTRING		"0, 6, 3, 1"
+#define NVSE_VERSION_INTEGER_BETA	0
+#define NVSE_VERSION_VERSTRING		"0, 6, 3, 0"
 #define NVSE_VERSION_PADDEDSTRING	"0006"
 
 // build numbers do not appear to follow the same format as with oblivion
