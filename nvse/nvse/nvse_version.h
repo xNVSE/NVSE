a857469a9c1b4e7d0851be8cd2346ee121a95d5a
korri123 <korri123@gmail.com>
2023-10-03 16:02:46 +0000

    Move console tokens to ExpressionEvaluator class

--- nvse/nvse/nvse_version.h
+++ nvse/nvse/nvse_version.h
@@ -6,7 +6,7 @@
 #define NVSE_VERSION_INTEGER		6
 #define NVSE_VERSION_INTEGER_MINOR	3
 #define NVSE_VERSION_INTEGER_BETA	4
-#define NVSE_VERSION_VERSTRING		"0, 6, 3, 3"
+#define NVSE_VERSION_VERSTRING		"0, 6, 3, 4"
 #define NVSE_VERSION_PADDEDSTRING	"0006"
 
 // build numbers do not appear to follow the same format as with oblivion
