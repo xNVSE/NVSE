b0f3e9d701a9119f306ba187470655daadcea2bb
Kormákur <korri123@gmail.com>
2025-12-08 22:03:16 +0000

    Update nvse_version.h

--- nvse/nvse/nvse_version.h
+++ nvse/nvse/nvse_version.h
@@ -5,8 +5,8 @@
 #define ALPHA_MODE 0
 #define NVSE_VERSION_INTEGER		6
 #define NVSE_VERSION_INTEGER_MINOR	4
-#define NVSE_VERSION_INTEGER_BETA	2
-#define NVSE_VERSION_VERSTRING		"0, 6, 4, 2"
+#define NVSE_VERSION_INTEGER_BETA	3
+#define NVSE_VERSION_VERSTRING		"0, 6, 4, 3"
 #define NVSE_VERSION_PADDEDSTRING	"0006"
 
 // build numbers do not appear to follow the same format as with oblivion
